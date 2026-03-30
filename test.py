ef19444b5ff948dc11a6b4d7f824c92dc96860e2
Himanshu kumar <himanshu231204@gmail.com>
2026-03-30 05:51:25 +0530

    chore: update files

--- test.py
+++ test.py
@@ -1,3 +1,17 @@
-a=10
-print(a)
-print(type(a))
+from ollama import chat
+
+stream = chat(
+    model="mistral",
+    messages=[{"role": "user", "content": "What is 17 × 23?"}],
+    stream=True,
+)
+
+content = ""
+
+for chunk in stream:
+    if chunk.message.content:
+        print(chunk.message.content, end="", flush=True)
+        content += chunk.message.content
+
+print("\n\nFinal Answer:", content)
+print("Done.")
